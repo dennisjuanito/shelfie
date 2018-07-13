9b106b60e367d79a92278d2a355c93d93341dd71
dennis juanito <dennisjuanito@denniss-MacBook-Pro-2.local>
2018-07-13 11:11:19 -0600

    error git

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard.js';
+import Header from './components/Header/Header.js';
+import Form from './components/Form/Form.js';
+import Product from './components/Product/Product.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Dashboard />
+        <Form />
+        <Product/>
       </div>
     );
   }
